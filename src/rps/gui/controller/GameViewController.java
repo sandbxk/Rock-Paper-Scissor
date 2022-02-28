5a654fab6912ba770ddfda882e6b6cefe0cbe45b
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-02-28 11:48:51 +0100

    created basic view

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,7 +1,12 @@
 package rps.gui.controller;
 
 // Java imports
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,11 +16,39 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
 
+    @FXML public Button btnRock;
+
+    @FXML public Button btnPaper;
+
+    @FXML public Button btnScissor;
+
+    @FXML public Button btnReset;
+
+    @FXML public Label lblHumanWins;
+
+    @FXML public Label lblBotWins;
+
+    @FXML public Label lblTies;
+
+    @FXML public Label lblRoundNumber;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    public void onRock(ActionEvent event) {
+    }
+
+    public void onPaper(ActionEvent event) {
+    }
+
+    public void onScissor(ActionEvent event) {
+    }
+
+    public void onReset(ActionEvent event) {
     }
 }
