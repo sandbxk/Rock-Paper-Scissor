ac97f01c00595abd8ef861373559785a1b89183f
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-03-01 22:19:47 +0100

    Animation complete i guess

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -9,10 +9,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Point3D;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 
 import java.net.URL;
@@ -46,8 +50,8 @@ public class GameViewController implements Initializable {
 
     private static final double DEFAULT_PLAYER_ROTATION = 0;
     private static final double DEFAULT_BOT_ROTATION = 7;
-    private static final double MAX_PLAYER_ROTATION = 40;
-    private static final double MAX_BOT_ROTATION = 47;
+    private static final double MAX_PLAYER_ROTATION = -30;
+    private static final double MAX_BOT_ROTATION = 37;
 
     private Image botRock;
     private Image botPaper;
@@ -57,17 +61,35 @@ public class GameViewController implements Initializable {
     private Image playerPaper;
     private Image playerScissor;
 
+    private Rotate playerRotate;
+    private Rotate botRotate;
+
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        imgViewBot.setRotate(DEFAULT_BOT_ROTATION);
-        imgViewPlayer.setRotationAxis(new Point3D(0, 0, (imgViewBot.getX() - imgViewBot.getFitWidth())));
+        //imgViewBot.setRotate(DEFAULT_BOT_ROTATION);
+        //imgViewPlayer.setRotationAxis(new Point3D(0, 0, (imgViewBot.getX() - imgViewBot.getFitWidth())));
        //imgViewBot.setRotationAxis(new Point3D(imgViewBot.getX() + imgViewBot.getX(), imgViewBot.getX() + imgViewBot.getX(), (imgViewBot.getX() + imgViewBot.getFitWidth())));
-        imgViewBot.setRotationAxis(new Point3D(0, 0,(imgViewBot.getX() + imgViewBot.getFitWidth())));
+        //imgViewBot.setRotationAxis(new Point3D(0, 0,(imgViewBot.getX() + imgViewBot.getFitWidth())));
+
+
         initImages();
+        initRotate();
+    }
+
+    private void initRotate(){
+        playerRotate = new Rotate();
+        playerRotate.setPivotX(imgViewPlayer.getX());
+        playerRotate.setPivotY(imgViewPlayer.getX() + (imgViewPlayer.getFitHeight() / 2));
+        imgViewPlayer.getTransforms().add(playerRotate);
+
+        botRotate = new Rotate(DEFAULT_BOT_ROTATION);
+        botRotate.setPivotX(imgViewBot.getX() + imgViewBot.getFitWidth());
+        botRotate.setPivotY(imgViewBot.getY() + (imgViewPlayer.getFitHeight() / 2));
+        imgViewBot.getTransforms().add(botRotate);
 
     }
 
@@ -84,7 +106,9 @@ public class GameViewController implements Initializable {
     }
 
     public void onRock(ActionEvent event) {
-        animateHands();
+        animateHands(200).setOnFinished(event1 -> {
+            imgViewBot.setImage(botPaper);
+        });
     }
 
     public void onPaper(ActionEvent event) {
@@ -97,7 +121,9 @@ public class GameViewController implements Initializable {
     }
 
 
-    private Timeline animateHands(){
+    private Timeline animateHands(int durationMillis){
+        imgViewBot.setImage(botRock);
+        imgViewPlayer.setImage(playerRock);
 
         Timeline raise1 = new Timeline();
         Timeline lower1 = new Timeline();
@@ -108,13 +134,14 @@ public class GameViewController implements Initializable {
         Timeline raise3 = new Timeline();
         Timeline lower3 = new Timeline();
 
-        Timeline raiseFinal = new Timeline();
-        Timeline lowerFinal = new Timeline();
+        Timeline raise4 = new Timeline();
+        Timeline lower4 = new Timeline();
 
-        KeyFrame lift1Player = new KeyFrame(Duration.millis(300), new KeyValue(imgViewPlayer.rotateProperty(), MAX_PLAYER_ROTATION));
-        KeyFrame lift1Bot = new KeyFrame(Duration.millis(300), new KeyValue(imgViewBot.rotateProperty(), MAX_BOT_ROTATION));
-        KeyFrame lower1Player = new KeyFrame(Duration.millis(300), new KeyValue(imgViewPlayer.rotateProperty(), DEFAULT_PLAYER_ROTATION));
-        KeyFrame lower1Bot = new KeyFrame(Duration.millis(300), new KeyValue(imgViewBot.rotateProperty(), DEFAULT_BOT_ROTATION));
+
+        KeyFrame lift1Player = new KeyFrame(Duration.millis(durationMillis), new KeyValue(playerRotate.angleProperty(), MAX_PLAYER_ROTATION));
+        KeyFrame lift1Bot = new KeyFrame(Duration.millis(durationMillis), new KeyValue(botRotate.angleProperty(), MAX_BOT_ROTATION));
+        KeyFrame lower1Player = new KeyFrame(Duration.millis(durationMillis), new KeyValue(playerRotate.angleProperty(), DEFAULT_PLAYER_ROTATION));
+        KeyFrame lower1Bot = new KeyFrame(Duration.millis(durationMillis), new KeyValue(botRotate.angleProperty(), DEFAULT_BOT_ROTATION));
 
         raise1.getKeyFrames().add(lift1Player);
         raise1.getKeyFrames().add(lift1Bot);
@@ -134,25 +161,29 @@ public class GameViewController implements Initializable {
         lower3.getKeyFrames().add(lower1Player);
         lower3.getKeyFrames().add(lower1Bot);
 
-        raiseFinal.getKeyFrames().add(lower1Player);
-        lowerFinal.getKeyFrames().add(lower1Bot);
+        raise4.getKeyFrames().add(lift1Player);
+        raise4.getKeyFrames().add(lift1Bot);
 
+        lower4.getKeyFrames().add(lower1Bot);
+        lower4.getKeyFrames().add(lower1Player);
 
         raise1.setOnFinished(event -> lower1.play());
         lower1.setOnFinished(event -> raise2.play());
         raise2.setOnFinished(event -> imageChangeOnAnimation(lower2, imgViewPlayer, imgViewBot, playerPaper, botPaper));
         lower2.setOnFinished(event -> raise3.play());
         raise3.setOnFinished(event -> imageChangeOnAnimation(lower3, imgViewPlayer, imgViewBot, playerScissor, botScissor));
-        lower3.setOnFinished(event -> imageChangeOnAnimation(raiseFinal, imgViewPlayer, imgViewBot, playerRock, botRock));
-        raiseFinal.setOnFinished(event -> lowerFinal.play());
+        lower3.setOnFinished(event -> raise4.play());
+        raise4.setOnFinished(event -> imageChangeOnAnimation(lower4, imgViewPlayer, imgViewBot, playerRock, botRock));
+
 
         raise1.play();
 
-        return lower3;
+        return lower4;
     }
 
     private void imageChangeOnAnimation(Timeline timeline, ImageView imgView1, ImageView imgView2, Image img1, Image img2){
         //Set hands to rock, paper, scissor
+
         imgView1.setImage(img1);
         imgView2.setImage(img2);
         timeline.play();
