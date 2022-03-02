34a9029f3fafb39e08adfd1c02da1b307d1d5c7b
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-03-02 12:17:43 +0100

    Merge branch 'main' of C:\Users\kaspe\Desktop\Rock-Paper-Scissor with conflicts.

--- src/rps/dal/ObjectIO.java
+++ src/rps/dal/ObjectIO.java
@@ -5,6 +5,7 @@ import rps.bll.game.Result;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ObjectIO {
 
@@ -17,22 +18,28 @@ public class ObjectIO {
         try (ObjectOutputStream ObjOut = new ObjectOutputStream(new FileOutputStream("statistic.txt"))) {
             results.addAll(readObject());
             results.add(result);
-            ObjOut.writeObject(result);
+            ObjOut.writeObject(results);
         } catch (IOException e)
         {
             e.printStackTrace();
         }
     }
 
-    public ArrayList<Result> readObject() throws FileNotFoundException {
-        ArrayList<Result> results = new ArrayList<>();
-        try (ObjectInputStream ObjIn = new ObjectInputStream(new FileInputStream("statistic.txt"))) {
-            if (ObjIn.readObject() != null)
-            results = (ArrayList<Result>) ObjIn.readObject();
-        } catch (IOException | ClassNotFoundException e ) {
-            e.printStackTrace();
+    public List<Result> readObject() throws FileNotFoundException {
+
+        try (ObjectInputStream ObjIn = new ObjectInputStream(new FileInputStream("statistic.txt")))
+        {
+               return (List<Result>) ObjIn.readObject();
+
         }
-        return results;
+        catch (IOException | ClassNotFoundException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ObjectIO IO = new ObjectIO();
+        System.out.println(IO.readObject());
     }
 
 }
